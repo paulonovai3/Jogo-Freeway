d28e7aec2f88bb52ea1e96db4d01e8c3f979392d
paulonovai3 <138393575+paulonovai3@users.noreply.github.com>
2023-07-02 15:08:11 -0300

    Create carro.js

---
+++ carro.js
@@ -0,0 +1,32 @@
+//carros
+// sempre começa com 0, 1, 2...
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [61, 87, 160, 228, 270, 300];
+let velocidadeCarros = [2, 2.4, 3.9, 5.6, 4.0, 2.3];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < - 50;
+}
